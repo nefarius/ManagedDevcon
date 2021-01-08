7a309192aa092ec111943858c40edb1d1c0fe20d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-08 16:37:42 +0100

    Added annotations

--- ManagedDevcon/Difx.cs
+++ ManagedDevcon/Difx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace Nefarius.Devcon
 {
@@ -9,16 +10,19 @@ namespace Nefarius.Devcon
             SetDifxLogCallback(logCallback, IntPtr.Zero);
         }
 
+        [UsedImplicitly]
         public uint Preinstall(string infPath, DifxFlags flags)
         {
             return DriverPackagePreinstall(infPath, (uint)flags);
         }
 
+        [UsedImplicitly]
         public uint Install(string infPath, DifxFlags flags, out bool rebootRequired)
         {
             return DriverPackageInstall(infPath, (uint)flags, (IntPtr)0, out rebootRequired);
         }
 
+        [UsedImplicitly]
         public uint Uninstall(string infPath, DifxFlags flags, out bool rebootRequired)
         {
             return DriverPackageUninstall(infPath, (uint)flags, (IntPtr)0, out rebootRequired);
